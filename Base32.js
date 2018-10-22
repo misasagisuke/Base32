291a63d08ff8ef42d8b235076c2076b708443461
和田陵佑 <wada-ryosuke@wadaryouyuunoMacBook-puro.local>
2018-10-23 00:21:53 +0900

    add split_5bit

--- Base32.js
+++ Base32.js
@@ -4,6 +4,12 @@ function testscript(str) {
 
     var list_binary = convert_binary(list_ascii);
     alert(list_binary)
+
+    var binary_str = list_binary.join('');
+    alert(binary_str);
+
+    var split5_list = split_5bit(binary_str);
+    alert(split5_list);
 }
 
 function convert_ascii(str) {
@@ -17,6 +23,29 @@ function convert_ascii(str) {
 function convert_binary(list) {
     for(var i = 0; i < list.length; i++) {
         list[i] = list[i].toString(2);
+    }
+    return list;
+}
+
+function split_5bit(str) {
+    var res = "";
+    var input_list = str.split('');
+    var list = new Array();
+    var index = 0;
+    for(var i = 0; i < input_list.length; i++) {
+        res = res + input_list[i];
+
+        if((i+1) % 5 == 0) {
+            list[index] = res;
+            index ++;
+            res = '';
+        } else if (i+1 == input_list.length) {
+            var sub = 5 - res.length;
+            for(var j = 0; j < sub; j++) {
+                res = res + '0';
+            }
+            list[index] = res;
+        }
     }
     return list;
 }
